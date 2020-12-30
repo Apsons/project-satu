06406db4d77efcd37a208beb469887f1c30948c7
Apsons <apsons16@gmail.com>
2020-12-30 12:42:44 +0700

    Added Challenge-3.js

--- challenge-3/Challenge-3.js
+++ challenge-3/Challenge-3.js
@@ -1,9 +1,26 @@
 // Challenge 3
 /* Implement the getInParallel Function that should be used to invoke multiple API calls in parallel. The function accepts an array of function that return a Promise. The function should return a Promise that should resolve to an arrayof result from the apiCalls argument. */
 
-function getInParallel(apiCalls) {
-    //Write your code here
-}
+// function getInParallel(apiCalls) {
+//     //Write your code here
+// }
+
+// async await
+async function getInParallel(apiCalls) {
+    // Write your code here
+    const results = [];
+  
+    for (const fn of apiCalls) {
+      try {
+        let result = await fn();
+        results.push(result);
+      } catch (err) {
+        results.push(err);
+      }
+    }
+  
+    return results;
+  }
 
 let promise = getInParallel([() => Promise.resolve("First API call!"), () => Promise.resolve("Second API call!")]);
 if(promise) {
