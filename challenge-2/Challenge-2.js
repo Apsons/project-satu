297b9e80d339d33351944b5a0a03cf702f2304a8
Apsons <apsons16@gmail.com>
2021-02-09 13:33:22 +0700

    personal update-2

--- challenge-2/Challenge-2.js
+++ challenge-2/Challenge-2.js
@@ -1,17 +1,16 @@
-// Challenge 2 
+// Challenge 2
 /*Implement the ensure function so that it throws error if called without arguments or the arguments is undifined. Otherwise it should return the given value */
 
 function ensure(value) {
-    if(value === undefined) {
-        throw new Error ("no arguments");
+    if (value === undefined) {
+      throw new Error("no arguments");
     }
-    return value
-}
-
-
-try {
-    console.log(ensure('test'));
-}catch(err) {
+    return value;
+  }
+  
+  try {
+    console.log(ensure());
+  } catch (err) {
     console.log(err);
-}
-
+  }
+  
